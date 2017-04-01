2f5a9c11b1fcb26f010984c1a62bff14a42da725
Chai Ching Ting <joliangel288@gmail.com>
2017-04-01 22:41:20 +0800

    2017.03.28

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.homework2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GoogleApiClient client;
+    private Button sayhello;
+    private View.OnClickListener goToSayHello = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            Intent open=new Intent();
+            open.setClass(MainActivity.this,Main2Activity.class);
+            startActivityForResult(open,2);
+        }
+    };
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent){
+        if(intent==null) return;
+        if(requestCode==2) {
+            String name=intent.getStringExtra("name");
+            Toast.makeText(MainActivity.this,"Hello "+name,Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        sayhello=(Button) findViewById(R.id.button);
+        sayhello.setOnClickListener(goToSayHello);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        else{
+            Uri url=Uri.parse("http://google.com.tw");
+            Intent geturl=new Intent(Intent.ACTION_VIEW,url);
+            startActivity(geturl);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
