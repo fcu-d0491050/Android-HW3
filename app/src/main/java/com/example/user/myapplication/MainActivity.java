678727a67e2c62c193698786e08a1ebe582d4583
Chai Ching Ting <joliangel288@gmail.com>
2017-04-01 22:56:01 +0800

    2017.03.21

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,46 +1,24 @@
-package com.example.homework2;
+package com.example.user.homework1;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GoogleApiClient client;
-    private Button sayhello;
-    private View.OnClickListener goToSayHello = new View.OnClickListener(){
-
-        @Override
-        public void onClick(View v) {
-            Intent open=new Intent();
-            open.setClass(MainActivity.this,Main2Activity.class);
-            startActivityForResult(open,2);
-        }
-    };
-
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent intent){
-        if(intent==null) return;
-        if(requestCode==2) {
-            String name=intent.getStringExtra("name");
-            Toast.makeText(MainActivity.this,"Hello "+name,Toast.LENGTH_LONG).show();
-
-        }
-    }
+    private TextView efInput;
+    private Button btnCacl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,67 +35,53 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
-        sayhello=(Button) findViewById(R.id.button);
-        sayhello.setOnClickListener(goToSayHello);
+        efInput =(EditText)findViewById(R.id.editText3);
+        btnCacl =(Button)findViewById(R.id.btn_submit);
+        btnCacl.setOnClickListener(say_hello);
     }
+    private  View.OnClickListener say_hello = new View.OnClickListener(){
+        public void onClick(View ad2){
+            String name = efInput.getText().toString();
+            Toast.makeText(MainActivity.this,"Hello "+name,Toast.LENGTH_SHORT).show();
+        }
+
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
-        else{
-            Uri url=Uri.parse("http://google.com.tw");
-            Intent geturl=new Intent(Intent.ACTION_VIEW,url);
-            startActivity(geturl);
+        else if (id == R.id.action_About){
+            AlertDialog.Builder MB =new AlertDialog.Builder(this);
+            MB.setTitle("About this APP");
+            MB.setMessage("Author:Chai Ching Ting");
+
+            DialogInterface.OnClickListener SU=new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface ad,int i){
+                }
+            };
+            MB.setPositiveButton("sure",SU);
+            MB.show();
+
         }
-
+        else if (id == R.id.action_Reset){
+            efInput.setText("");
+        }
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Main Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
